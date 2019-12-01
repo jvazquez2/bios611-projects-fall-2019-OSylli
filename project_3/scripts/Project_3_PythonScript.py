1c0a88ffc78d7776ee58e49196841b5e145c0178
OSylli <yilunli@email.unc.edu>
2019-11-30 19:50:49 -0500

    Project 3 Scripts

--- project_3/scripts/Project_3_PythonScript.py
+++ project_3/scripts/Project_3_PythonScript.py
@@ -5,6 +5,7 @@
 
 
 # Question 2: The relationship between duration of stay at UMD and clients' gender and race
+# Data read in
 import pandas as pd
 client = pd.read_csv('https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-OSylli/master/project_3/data/CLIENT_191102.tsv',sep='\t')
 client = client.loc[:,['Client ID', 'Client Gender', 'Client Primary Race']]
@@ -14,6 +15,7 @@ client.head()
 # In[2]:
 
 
+# Data read in
 import pandas as pd
 entry_exit = pd.read_csv('https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-OSylli/master/project_3/data/ENTRY_EXIT_191102.tsv',sep='\t')
 entry_exit = entry_exit.loc[:,['Client ID', 'Entry Date', 'Exit Date']]
@@ -23,85 +25,101 @@ entry_exit.head()
 # In[3]:
 
 
+# Load necessary packages
 import pandas as pd
 
+# Since the column "Client ID" are matched in the two datasets, we directly combine them together
 record=pd.concat([client, entry_exit],axis=1)
+
+# Change the format of date record and calculate the duration of stay at UMD of each item
 record['Entry Date'] = pd.to_datetime(record['Entry Date'],format='%m/%d/%Y')
 record['Exit Date'] = pd.to_datetime(record['Exit Date'],format='%m/%d/%Y')
 record['Duration (Days)'] = record['Exit Date']-record['Entry Date']
-
 record['Duration (Days)'] = record['Duration (Days)'].map(lambda x: x.days)
 
+# Drop imcomplete record
 record = record.dropna(axis = 0, how ='any')
 
+# Output
 record.head()
 
 
 # In[4]:
 
 
-# Relationship between the duration of stay at UMD and Client Gender
+# Question 2 Part 1: Relationship between the duration of stay at UMD and Client Gender
 gender_record = {'Gender':[], 'Count':[], 'AVE_duration':[]}
 
+# Data wrangling to calculate the number and average time of stay at UMD for different gender
 for value, sub_df in record.groupby('Client Gender'):
     tmp_ave = sub_df['Duration (Days)'].mean()
     gender_record['Gender'].append(value)
     gender_record['Count'].append(sub_df['Duration (Days)'].count())
     gender_record['AVE_duration'].append(tmp_ave)
     
+# Change the dictionary in to a dataframe and print it out
 gender_record = pd.DataFrame(gender_record)
 gender_record
 
 
-# In[7]:
+# In[12]:
 
 
+# Load necessary packages for visualization
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
 
+# Generate a barplot to show relationship between the total number of entry and client gender
 plt.bar(gender_record['Gender'],gender_record['Count'])
 plt.title('Relationship between the total number of entry and client gender')
-savefig("Q2_Relationship between the total number of entry and client gender.png")
+savefig("Q2_Relationship between the total number of entry and client gender.png", bbox_inches = 'tight')
 
 
-# In[8]:
+# In[13]:
 
 
+# Load necessary packages for visualization
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
 
+# Generate a barplot to show the relationship between the average time of stay at UMD and client gender
 plt.bar(gender_record['Gender'],gender_record['AVE_duration'])
 plt.title('Relationship between the average time of stay at UMD and client gender')
-savefig("Q2_Relationship between the average time of stay at UMD and client gender.png")
+savefig("Q2_Relationship between the average time of stay at UMD and client gender.png", bbox_inches = 'tight')
 
 
 # In[9]:
 
 
-# Relationship between the duration of stay at UMD and Client Race
+# Part 2: Relationship between the duration of stay at UMD and Client Race
 race_record = {'Race':[], 'Count':[], 'AVE_duration':[], 'Number':[]}
 
+# Data wrangling
 for value, sub_df in record.groupby('Client Primary Race'):
     tmp_ave = sub_df['Duration (Days)'].mean()
     race_record['Race'].append(value)
     race_record['Count'].append(sub_df['Duration (Days)'].count())
     race_record['AVE_duration'].append(tmp_ave)
     
+# Drop unnecessary summarized statistics    
 race_record["Number"] = [0,1,2,3,4,5,6,7]
 race_record = pd.DataFrame(race_record)
 race_record = race_record.drop(5,axis=0) # drop the row in which "Data not collected" is recorded
 race_record = race_record.drop(4,axis=0) # drop the row in which "Client refused" is recorded
 race_record = race_record.drop(3,axis=0) # drop the row in which "Client doesn't know" is recorded
 
+# Output
 race_record
 
 
 # In[10]:
 
 
+# Load necessary Packages
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
 
+# Visualization with a barplot, shwoing the relationship between the total number of entry and client race
 LABELS = ["American Indian or Alaska Native","Asian","Black or African American","Native Hawaiian or Other Pacific Islander","White"]
 
 plt.bar(race_record['Number'],race_record['Count'])
@@ -114,10 +132,11 @@ savefig("Q2_Relationship between the total number of entry and clients\' race.pn
 # In[11]:
 
 
+# Load necessary packages
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import savefig
-#race = [0,1,2,6,7]
 
+# Visualization with a barplot, showing the relationship between the average time of stay at UMD and client race
 LABELS = ["American Indian or Alaska Native","Asian","Black or African American","Native Hawaiian or Other Pacific Islander","White"]
 
 
--- project_3/scripts/Project_3_Rscript.R
+++ project_3/scripts/Project_3_Rscript.R
@@ -1,33 +1,41 @@
+#---Load Necessary Packages---
 library(tidyverse)
 library(ggplot2)
 install.packages("data.table",dependencies = TRUE,repos='http://cran.us.r-project.org')
 library(data.table)
 
-# Question 1: The trend of the number of entry to UMD over years and the possible seasonal characteristics of the number of entry.
+#---Question 1: The trend of the number of entry to UMD over time and the possible seasonal characteristics of the number of entry---
+#---Date read in & Format of the recording of dates changed---
 EntryExit<-read.delim(url("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-OSylli/master/project_3/data/ENTRY_EXIT_191102.tsv"))
 EntryExit$Entry.Date<-as.Date(EntryExit$Entry.Date,format="%m/%d/%Y")
 EntryExit$Exit.Date<-as.Date(EntryExit$Exit.Date,format="%m/%d/%Y")
-Entry_Date<-as.data.frame(EntryExit) %>%
-  filter(Entry.Date>="2014-07-01") %>%
-  select("Entry.Date") %>%
-  mutate(year=year(Entry.Date)) %>%
-  mutate(month=month(Entry.Date))
 
+#---Data wrangling---
+Entry_Date<-as.data.frame(EntryExit) %>%
+  filter(Entry.Date>="2014-07-01") %>%      # Drop imcomplete records and only keep the records after "2014-07-01"
+  select("Entry.Date") %>%                  # Focus on the date of entry
+  mutate(year=year(Entry.Date)) %>%         # Add new column "year" to see the trend of the number of entry to UMD over time
+  mutate(month=month(Entry.Date))           # Add new column "month" to see the possible seasonal characteristics of the number of entry
+
+#---Define a new data frame with columns "year", "month" and "count"---
 EntryTrend<-Entry_Date %>%
   group_by(year,month) %>%
   summarise(count=n()) 
 
+#---Build a time series and make a time series plot, in order to figure out its trend over time---
 TS_EntryTrend<-ts(EntryTrend$count,frequency=12,start=c(2014,7))
 TS_EntryTrend
 ts.plot(TS_EntryTrend,ylab="Occurence")
 points(TS_EntryTrend)
 title("Time Series Plot: Trend of Entry to UMD from Jul 2014 to Oct 2019")
 
+#---Build another data frame which focus on the number of entry in different months---
 MonthlyEntry<-Entry_Date %>%
   group_by(month) %>%
   summarise(count=n())
 MonthlyEntry$month<-as.factor(MonthlyEntry$month)
 
+#---Visualization with ggplot---
 ggplot(MonthlyEntry,aes(x=month,y=count))+
   geom_bar(stat="identity",width=0.8,colour="black",fill="dark blue")+
   theme_minimal()+
@@ -42,23 +50,31 @@ ggplot(MonthlyEntry,aes(x=month,y=count))+
 
 
 
-# Question 3: The relationship between duration of stay at UMD and clients' age, live condition and Housing status at the time of entry.
+#---Question 3: The relationship between duration of stay at UMD and clients' age, live condition and Housing status at the time of entry---
+#---Data read in---
 CLIENT<-read.delim(url("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-OSylli/master/project_3/data/CLIENT_191102.tsv"))
 QUESTION<-read.delim(url("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-OSylli/master/project_3/data/EE_UDES_191102.tsv"))
 
-# Since the column "Client ID" in the three tables "CLIENT", "QUESTION" and "EntryExit" are already matched, we can use rbind() directly to combine the datasets
+#---Since the column "Client ID" in the three tables "CLIENT", "QUESTION" and "EntryExit" are already matched, we can use rbind() directly to combine the datasets---
 record<-cbind(CLIENT,QUESTION,EntryExit)
-record<-record[,-c(11,13,14,15,32,33,34,35)]
+record<-record[,-c(11,13,14,15,32,33,34,35)]    # Drop the repeated columns
+
+#---Basic wrangling---
 record<-record %>%
+  # Add new column to calculate the duration of stay at UMD
   mutate(duration=Exit.Date-Entry.Date) %>%
+  # Select columns of interest and rename with simpler label
   select(Client.ID, Client.Age.at.Entry, Prior.Living.Situation.43., Housing.Status.2703., Does.the.client.have.a.disabling.condition..1935., duration) %>%
   rename(ID="Client.ID",age="Client.Age.at.Entry",LiveCondition="Prior.Living.Situation.43.",Housing="Housing.Status.2703.",DISABLE="Does.the.client.have.a.disabling.condition..1935.")
 
+#---Part 1: Relationship between duration of stay at UMD and clients' age---
+# Data wrangling
 age_duration<-record %>%
   drop_na() %>%
   group_by(age) %>%
   summarise(Count=n(),AVE_Dur=mean(duration))
 
+# Visualization with ggplot
 ggplot(age_duration,aes(x=age,y=AVE_Dur))+
   geom_point()+
   geom_smooth()+
@@ -67,12 +83,16 @@ ggplot(age_duration,aes(x=age,y=AVE_Dur))+
        title="The relationship between duration of stay and clients' age")+
   theme_minimal()
 
+
+#---Part 2: Relationship between duration of stay at UMD and clients' living condition---
+# Data wrangling
 livecon_duration<-record %>%
   drop_na() %>%
   filter(LiveCondition!=""&LiveCondition!="Client doesn't know (HUD)"&LiveCondition!="Client refused (HUD)"&LiveCondition!="Data not collected (HUD)") %>%
   group_by(LiveCondition) %>%
   summarise(Count=n(),AVE_Dur=mean(duration))
 
+# Visualization with a histogram
 ggplot(livecon_duration,aes(x=LiveCondition,y=AVE_Dur))+
   geom_histogram(stat="identity",colour="black",fill="dark blue")+
   labs(x="Live Condition",
@@ -103,6 +123,7 @@ ggplot(livecon_duration,aes(x=LiveCondition,y=AVE_Dur))+
                            "Transitional housing for homeless persons"))+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+# Generate another histogram to help to see the relationship between the number of the entry and clients' living condition
 ggplot(livecon_duration,aes(x=LiveCondition,y=Count))+
   geom_histogram(stat="identity",colour="black",fill="dark blue")+
   labs(x="Live Condition",
@@ -133,12 +154,16 @@ ggplot(livecon_duration,aes(x=LiveCondition,y=Count))+
                            "Transitional housing for homeless persons"))+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+
+#---Part 3: Relationship between duration of stay at UMD and clients' housing status---
+# Data wrangling
 housing_duration<-record %>%
   drop_na() %>%
   filter(Housing!=""&Housing!="Client doesn't know (HUD)"&Housing!="Client refused (HUD)") %>%
   group_by(Housing) %>%
   summarise(Count=n(),AVE_Dur=mean(duration))
 
+# Visualization with a histogram
 ggplot(housing_duration,aes(x=Housing,y=AVE_Dur))+
   geom_histogram(stat="identity",colour="black",fill="dark blue")+
   theme_minimal()+
